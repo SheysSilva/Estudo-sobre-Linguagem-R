efb7ae91c4c0a290c73c29f04650dcb9e2888bed
SheysSilva <sheilla.silva@ccc.ufcg.edu.br>
2017-10-31 08:48:12 -0300

    Add files via upload

---
+++ criando_variaveis.R
@@ -0,0 +1,16 @@
+#Variavéis
+
+#Atribuindo valores / criando variavéis
+
+variavel <- 10
+variavel # retorna o valor que a variável possui
+
+#Atribuindo outro tipo de valor em "variavel"
+variavel <- "comida"
+variavel
+
+#Resultado de um cálculo
+x <- 20 ^ 2
+x
+x / 4
+x
---
+++ quatro_operacoes_matematicas_basicas.R
@@ -0,0 +1,24 @@
+#Comentário
+#Tem que selecionar a linha, ou as linhas, para poder executar
+#Quatro operações básicas 
+
+#Adição
+4 + 5
+
+#Subtração
+4 - 5
+
+#Multiplicação
+4 * 5
+
+#Exponenciação
+4 ^ 5
+
+#Resto da divisão
+4 %% 5
+
+#Ordem de parenteses
+(4 * (5 + 1))
+
+#Ordem matematica
+4 * 5 + 1
