b00603181728c7537d8df801c038f66739c48d23
SheysSilva <sheilla.silva@ccc.ufcg.edu.br>
2017-11-08 09:32:20 -0300

    Add files via upload

---
+++ estatistica_descritiva.R
@@ -0,0 +1,22 @@
+#Estatística descritiva
+
+#Medidas de Posição
+	#Média
+		#(x1 + x2 + ... + xn) / n
+		#Não utilizar se tiver um valor fora do padrão
+		#Ex: (1, 12, 3, 230, 10, 3, 22) Obs: O valor 230 está fora do padrão
+	#Moda
+	#Máximo e Mínimo
+	#Mediana
+	#Quantis
+
+#Medidas de Dispersão
+	#Variância e Desvio - Padrão
+	#Coeficiente de Variação
+
+#Gráficos
+	#Histograma
+	#Boxplot
+	#Dispersão
+	#Pizza
+	#Barras
